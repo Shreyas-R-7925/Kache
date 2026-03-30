dc2502bf8668e6f4a13bab2cf5f98b5ac98d1ca9
Shreyas R <shreyasr@MacBook-Air.local>
2026-03-30 23:44:59 +0530

    feat: implement TTL with lazy and active expiration

--- include/InMemoryStorage.h
+++ include/InMemoryStorage.h
@@ -2,16 +2,36 @@
 #include "StorageEngine.h"
 #include <string>
 #include <unordered_map>
+#include <optional>
+#include <chrono>
+#include <thread>
+
+using TimePoint = std::chrono::steady_clock::time_point;
 
 class InMemoryStorage : public StorageEngine {
 public:
-    ~InMemoryStorage() = default;
+    InMemoryStorage(); 
+    ~InMemoryStorage();
 
-    std::string get(const std::string& key) const override;
+    std::optional<std::string> get(const std::string& key) const override;
     void set(const std::string& key, const std::string& value) override;
+    void set(const std::string& key, const std::string& value, std::chrono::seconds ttl) override;
     void del(const std::string& key) override;
     bool exists(const std::string& key) const override;
 
 private:
-    std::unordered_map<std::string, std::string> store;
+
+    mutable std::mutex mtx;              
+    std::thread cleanupThread;          
+    std::atomic<bool> stopFlag{false};  
+
+    void cleanupExpiredKeys(); 
+
+    struct Entry {
+        std::string value;
+        std::optional<TimePoint> expiry;
+    };
+
+    std::unordered_map<std::string, Entry> store;
+    
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,26 @@
 #include "../include/InMemoryStorage.h"
 #include <iostream>
+#include <thread>
 
 int main() {
     InMemoryStorage storage;
 
-    storage.set("name", "Kache");
-    storage.set("version", "1.0");
+    storage.set("name", "shreyas");
+    auto val1 = storage.get("name");
 
-    if (storage.exists("name")) {
-        std::string name = storage.get("name");
-        std::cout << "Name: " << name << std::endl;
+    if (val1) {
+        std::cout << "name: " << *val1 << std::endl;
     }
 
-    storage.del("version");
+    storage.set("temp", "123", std::chrono::seconds(2));
 
-    if (!storage.exists("version")) {
-        std::cout << "Version key deleted successfully." << std::endl;
-    }
+    auto val2 = storage.get("temp");
+    std::cout << "temp before expiry: " << (val2 ? *val2 : "null") << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    auto val3 = storage.get("temp");
+    std::cout << "temp after expiry: " << (val3 ? *val3 : "null") << std::endl;
 
     return 0;
 }
