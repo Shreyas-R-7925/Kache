0b9cfd880438d2e27ad724eb09a12577f0ae8f3f
Shreyas R <shreyasr@MacBook-Air.local>
2026-04-02 00:11:12 +0530

    Implemented LRU eviction policy

--- include/StorageEngine.h
+++ include/StorageEngine.h
@@ -6,7 +6,7 @@ class StorageEngine {
 public:
     virtual ~StorageEngine() = default; 
 
-    virtual std::optional<std::string> get(const std::string& key) const = 0;
+    virtual std::optional<std::string> get(const std::string& key) = 0;
     virtual void set(const std::string& key, const std::string& value) = 0;
     virtual void set(const std::string& key, const std::string& value, std::chrono::seconds ttl) = 0;
     virtual void del(const std::string& key) = 0;
--- src/InMemoryStorage.cpp
+++ src/InMemoryStorage.cpp
@@ -1,6 +1,16 @@
-#include "../include/InMemoryStorage.h"
+#include "../include/InMemoryStorage.h"   
+#include <stdexcept>
+#include "constants/KacheConstants.h"
+#include "../include/LRUPolicy.h"
 
-InMemoryStorage::InMemoryStorage() {
+InMemoryStorage::InMemoryStorage(size_t cap, const std::string& policyType) : capacity(cap) {
+
+    if (policyType == LRU_EVICTION_POLICY) {
+        policy = std::make_unique<LRUPolicy>();
+    } else {
+        throw std::invalid_argument("Unsupported eviction policy");
+    }
+
     cleanupThread = std::thread(&InMemoryStorage::cleanupExpiredKeys, this);
 }
 
@@ -12,54 +22,78 @@ InMemoryStorage::~InMemoryStorage() {
     }
 }
 
-std::optional<std::string> InMemoryStorage::get(const std::string& key) const {
-
-    std::lock_guard<std::mutex> lock(mtx); 
+std::optional<std::string> InMemoryStorage::get(const std::string& key) {
+    std::lock_guard<std::mutex> lock(mtx);
 
     auto it = store.find(key);
-    if (it == store.end()) {
+    if (it == store.end()) return std::nullopt;
+
+    auto now = std::chrono::steady_clock::now();
+
+    if (it->second.expiry && now > it->second.expiry.value()) {
+        policy->onDelete(key);
+        store.erase(it);
         return std::nullopt;
     }
 
-    if (it->second.expiry && std::chrono::steady_clock::now() > it->second.expiry.value()) {
-        return std::nullopt;
-    }
-
+    policy->onGet(key); 
     return it->second.value;
 }
 
 void InMemoryStorage::set(const std::string& key, const std::string& value) {
-
     std::lock_guard<std::mutex> lock(mtx);
 
+    if (store.find(key) != store.end()) {
+        store[key].value = value;
+        policy->onSet(key);
+        return;
+    }
+
+    if (store.size() >= capacity) {
+        std::string evictKey = policy->evict();
+        store.erase(evictKey);
+    }
+
     store[key] = {value, std::nullopt};
+    policy->onSet(key);
 }
 
 void InMemoryStorage::set(const std::string& key, const std::string& value, std::chrono::seconds ttl) {
-
-    std::lock_guard<std::mutex> lock(mtx); 
+    std::lock_guard<std::mutex> lock(mtx);
 
     TimePoint expiry = std::chrono::steady_clock::now() + ttl;
+
+    if (store.find(key) != store.end()) {
+        store[key] = {value, expiry};
+        policy->onSet(key);
+        return;
+    }
+
+    if (store.size() >= capacity) {
+        std::string evictKey = policy->evict();
+        store.erase(evictKey);
+    }
+
     store[key] = {value, expiry};
+    policy->onSet(key);
 }
 
 void InMemoryStorage::del(const std::string& key) {
-
-    std::lock_guard<std::mutex> lock(mtx); 
+    std::lock_guard<std::mutex> lock(mtx);
 
     store.erase(key);
+    policy->onDelete(key);  
 }
 
 bool InMemoryStorage::exists(const std::string& key) const {
-
-    std::lock_guard<std::mutex> lock(mtx); 
+    std::lock_guard<std::mutex> lock(mtx);
 
     auto it = store.find(key);
-    if (it == store.end()) {
-        return false;
-    }
+    if (it == store.end()) return false;
 
-    if (it->second.expiry && std::chrono::steady_clock::now() > it->second.expiry.value()) {
+    auto now = std::chrono::steady_clock::now();
+
+    if (it->second.expiry && now > it->second.expiry.value()) {
         return false;
     }
 
@@ -74,7 +108,10 @@ void InMemoryStorage::cleanupExpiredKeys() {
             for (auto it = store.begin(); it != store.end();) {
                 if (it->second.expiry &&
                     std::chrono::steady_clock::now() > it->second.expiry.value()) {
+
+                    policy->onDelete(it->first); 
                     it = store.erase(it);
+
                 } else {
                     ++it;
                 }
